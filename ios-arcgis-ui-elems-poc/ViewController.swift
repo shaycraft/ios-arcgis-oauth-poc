89e752e9a24170da041aa6c84ea58c3fe77f3073
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-04-02 21:38:09 -0600

    feat: implements web view

--- ios-arcgis-ui-elems-poc/ViewController.swift
+++ ios-arcgis-ui-elems-poc/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 import ArcGIS
 
-class ViewController: UIViewController, AGSGeoViewTouchDelegate {
+class ViewController: UIViewController, AGSGeoViewTouchDelegate, WKNavigationDelegate {
     @IBOutlet weak var mapView: AGSMapView!
     @IBOutlet weak var debugLabel: UILabel!
+    @IBOutlet weak var webView: WKWebView!
+    
+    private var _firstNav: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +34,12 @@ class ViewController: UIViewController, AGSGeoViewTouchDelegate {
         self.mapView.touchDelegate = self
         
         self.mapView.setViewpoint(zoomPoint)
+        
+        self.mapView.isHidden = true
+        
+        self.webView.load(URLRequest(url: URL(string: "https://google.com")!))
+        
+        self.webView.navigationDelegate = self
     }
     
     
@@ -42,4 +51,17 @@ class ViewController: UIViewController, AGSGeoViewTouchDelegate {
         self.debugLabel.text = "Lat/long = {\( String(format: "%.3f", projectPoint.x)), \(String(format: "%.3f", projectPoint.y)) }"
     }
     
+    func webView( _ webView: WKWebView, didFinish navigation: WKNavigation! ) {
+        print(">>>>>>> DEBUG: Navigation detected, url = \(self.webView.url!.absoluteString)")
+        if (!self._firstNav) {
+            self.mapView.isHidden = false
+            self.webView.isHidden = true
+            return
+        }
+        print("DEBUG:  navigation detected...")
+        
+        self._firstNav = false
+    }
+    
 }
+
