f78b4c0259d87a66528e1072f90b36e762ad9e20
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-04-09 00:02:02 -0600

    chore: adds import

--- ios-arcgis-oauth-poc/ViewController.swift
+++ ios-arcgis-oauth-poc/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import ArcGIS
+import AuthenticationServices
 
 extension ViewController: ASWebAuthenticationPresentationContextProviding {
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
